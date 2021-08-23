b92a80780e839dbbb4d702cc18bb7e3ff4553402
Kennedy Kithome <kithomeken@gmail.com>
2021-08-23 22:13:26 +0300

    Guest Routes Switch Component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,21 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Sanctum } from 'react-sanctum'
 import axios from 'axios'
 
+import Reducer from './store/Reducer'
 
+interface GuestRouteInterface {
+    path: string;
+    component: any;
+    exact?: boolean;
+}
+
+const store = createStore(Reducer, applyMiddleware(thunk))
+let protectedRoutes: Array<any> = []
+
+const guestRoutes: Array<GuestRouteInterface> = [
+    { path: "/auth/sign-in", component: SignIn, exact: true },
+    { path: "/auth/password-recovery", component: ForgotPassword, exact: true },
+];
 
 const sanctumConfig = {
     apiUrl: "https://project-0.kennedykitho.me",
@@ -23,7 +37,16 @@ export default class App extends Component {
             <Sanctum config={sanctumConfig}>
                 <BrowserRouter>
                     <Switch>
-                        <p>Hello, welcome to project Odessa</p>
+                        {guestRoutes.map((route, key) => {
+                            return (
+                                <Route
+                                    // exact={route.exact}
+                                    path={route.path}
+                                    component={route.component}
+                                    key={key}
+                                />
+                            )
+                        })}
                     </Switch>
                 </BrowserRouter>
             </Sanctum>
