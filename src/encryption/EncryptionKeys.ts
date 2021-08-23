0b9733c8fa35eb5887a4edd23748ad048ecbea29
Kennedy Kithome <kithomeken@gmail.com>
2021-08-23 22:50:36 +0300

    Encryption

---
+++ src/encryption/EncryptionKeys.ts
@@ -0,0 +1,11 @@
+class EncryptionKeys {
+    keyBase() {
+        return 'BO/h^_iaJ[Yv(+)p<OZ38YcJ+<c[~y'
+    }
+
+    ivBase() {
+        return '8E2I).z~.W{KG%en:=J(h}j_d"bPR&'
+    }
+}
+
+export default new EncryptionKeys()
