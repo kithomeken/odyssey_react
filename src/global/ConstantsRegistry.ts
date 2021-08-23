0659a00b5e580dbac48e039577db7bc241b5f2fb
Kennedy Kithome <kithomeken@gmail.com>
2021-08-23 22:51:28 +0300

    Global Constants Registry

---
+++ src/global/ConstantsRegistry.ts
@@ -0,0 +1,11 @@
+class ConstantsRegistry {
+    cookieNameForAccessToken() {
+        return '_ac.tx'
+    }
+
+    cookieNameForUserName() {
+        return '_fl.nm'
+    }
+}
+
+export default new ConstantsRegistry()
