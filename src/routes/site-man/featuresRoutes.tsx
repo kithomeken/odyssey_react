cd35d4b3f1e40d61999e3ff907ce7f99700024e2
Kennedy Kithome <kithomeken@gmail.com>
2021-08-24 19:44:17 +0300

    Prototype II Purge Routes

--- src/routes/site-man/featuresRoutes.tsx
+++
@@ -1,5 +0,0 @@
-import SupportFeature from "../../views/site-man/features/SupportFeatures"
-
-export const featuresRoutes = [
-    { path: "/a/default/site-manager/features/support", component: SupportFeature, exact: true, activeMenu: 'support' },
-]
--- src/routes/site-man/securityRoutes.tsx
+++
@@ -1,7 +0,0 @@
-import AccessTeams from "../../views/site-man/security/AccessTeams"
-import CreateAccessTeam from "../../views/site-man/security/CreateAccessTeam"
-
-export const securityRoutes = [
-    { path: "/a/default/site-manager/security/access-teams", component: AccessTeams, exact: true, activeMenu: 'teams' },
-    { path: "/a/default/site-manager/security/access-teams/create", component: CreateAccessTeam, exact: true, activeMenu: 'teams' },
-]
