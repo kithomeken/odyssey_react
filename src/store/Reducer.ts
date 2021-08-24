3090e68ae08c854d51cbf40bc4670215d2f09d06
Kennedy Kithome <kithomeken@gmail.com>
2021-08-24 19:44:44 +0300

    Prototype II Purge Redux Store

--- src/store/Reducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux"
-import userState from "./UserStore"
-import activityState from "./ActivityState"
-
-export default combineReducers({
-    userState,
-    activityState
-})
