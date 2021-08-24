f6559984437a6e6720d0782894f0c6d4b74ab46d
Kennedy Kithome <kithomeken@gmail.com>
2021-08-25 00:10:48 +0300

    View Prot II

---
+++ src/views/Home.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>This is home</p>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Home
---
+++ src/views/authentication/SignIn.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class SignIn extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>This is sign in</p>
+            </React.Fragment>
+        )
+    }
+}
+
+export default SignIn
