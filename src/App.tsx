afde99144ad38b9456d465919d47519a1d599914
Kennedy Kithome <kithomeken@gmail.com>
2021-08-25 00:08:36 +0300

    Protected Routes Prot II

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Sanctum } from 'react-sanctum'
 import axios from 'axios'
 
+import Reducer from './store/Reducers'
+import { ProtectedRoutes } from './components/router/ProtectedRoutes'
+
+// Components
 import SignIn from './views/authentication/SignIn'
+import FromIndexToHome from './Redirects/FromIndexToHome'
+import Home from './views/Home'
 
 interface GuestRouteInterface {
     path: string;
@@ -16,7 +22,17 @@ interface GuestRouteInterface {
 
 const guestRoutes: Array<GuestRouteInterface> = [
     { path: "/auth/sign-in", component: SignIn, exact: true },
-];
+]
+
+const temporaryRoutes = [
+    { path: "/", component: FromIndexToHome, exact: true, activeMenu: 'Y' },
+    { path: "/home", component: Home, exact: true, activeMenu: 'Y' },
+]
+const store = createStore(Reducer, applyMiddleware(thunk))
+let protectedRoutes: Array<any> = []
+
+protectedRoutes = temporaryRoutes.concat(
+);    
 
 const sanctumConfig = {
     apiUrl: "https://project-0.kennedykitho.me/",
@@ -36,13 +52,29 @@ export default class App extends Component {
                     {guestRoutes.map((route, key) => {
                             return (
                                 <Route
-                                    // exact={route.exact}
+                                    exact={route.exact}
                                     path={route.path}
                                     component={route.component}
                                     key={key}
                                 />
                             )
                         })}
+
+                        <Provider store={store}>
+                            {
+                                protectedRoutes.map((route, key) => {
+                                    return (
+                                        <ProtectedRoutes
+                                            exact={route.exact}
+                                            path={route.path}
+                                            component={route.component}
+                                            activeMenu={route.activeMenu}
+                                            key={key}
+                                        />
+                                    )
+                                })
+                            }
+                        </Provider>
                     </Switch>
                 </BrowserRouter>
             </Sanctum>
