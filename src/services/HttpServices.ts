f915ea3e47e10eb79148dc5a56cf6c259ba75a49
Kennedy Kithome <kithomeken@gmail.com>
2021-08-25 21:01:51 +0300

    Http Services

--- src/services/HttpServices.ts
+++ src/services/HttpServices.ts
@@ -1,9 +1,79 @@
 import axios from "axios"
+import Crypto from "../encryption/Crypto"
+
+import ConstantsRegistry from "../global/ConstantsRegistry"
+import CookieService from "./CookieService"
 
 class HttpServices {
-    async getData() {
+    protected decryptAccessTokenCookie() {
+        const cookieNameForAccessToken = ConstantsRegistry.cookieNameForAccessToken()
+        const cipherText = CookieService.get(cookieNameForAccessToken)
+        let accessToken = null
 
+        if (cipherText != null) {
+            accessToken = Crypto.decryptDataUsingAES256(cipherText)
+        }
+
+        return accessToken
+    }
+
+    async httpGet(url: string) {
+        const accessToken = this.decryptAccessTokenCookie()
+        const authorizationBearer = {
+            headers: {
+                Authorization: "Bearer " + accessToken,
+            }
+        }
+
+        try {
+            return await axios.get(url, authorizationBearer)
+        } catch (error) {
+            console.log("Could not fetch data", error)
+        }
+    }
+
+    async httpPost(url:string, data: any, options: any = null) {
+        const accessToken = this.decryptAccessTokenCookie()
+        const authorizationBearer = {
+            headers: {
+                Authorization: "Bearer " + accessToken,
+            }
+        }
+
+        try {
+            const finalOptions = Object.assign(authorizationBearer, options)
+            return await axios.post(url, data, finalOptions);
+        } catch (error: any) {
+            console.error("Could not post data", error);
+            return (error.response !== undefined) ? error.response : error;
+        }
+    }
+
+    async httpPostWithoutData(url:string, data: any = null, options: any = null) {
+        const accessToken = this.decryptAccessTokenCookie()
+        const authorizationBearer = {
+            headers: {
+                Authorization: "Bearer " + accessToken,
+            }
+        }
+
+        try {
+            const finalOptions = Object.assign(authorizationBearer, options)
+            return await axios.post(url, data, finalOptions);
+        } catch (error: any) {
+            console.error("Could not post data", error);
+            return (error.response !== undefined) ? error.response : error;
+        }
+    }
+
+    axiosInstanceHeaders() {
+        const accessToken = this.decryptAccessTokenCookie()
+        const authorizationBearer = {
+            Authorization: "Bearer " + accessToken,
+        }
+
+        return authorizationBearer
     }
 }
 
-export default HttpServices
+export default new HttpServices()
