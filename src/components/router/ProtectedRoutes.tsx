bdd435c03c96bcafdb4bd7b934b253ff67a74aca
Kennedy Kithome <kithomeken@gmail.com>
2021-09-15 11:47:39 +0300

    Protected Routes Components

--- src/components/router/ProtectedRoutes.tsx
+++ src/components/router/ProtectedRoutes.tsx
@@ -2,6 +2,9 @@ import React, {FC} from "react"
 import {Route, Redirect} from 'react-router-dom'
 import Auth from './Auth'
 
+import ParamsNavigation from '../layouts/parameters/Navigation'
+import ParamsUserBox from '../layouts/parameters/UserBox'
+
 interface Props {
     exact: boolean,
     path: string,
@@ -16,10 +19,27 @@ export const ProtectedRoutes: FC<Props> = ({component: Component, activeMenu, ..
             render={
                 (props) => {
                     if (Auth.isAuthenticated()) {
+                        const marginLeft = {marginLeft: '288px'}
+                        const marginTop = {marginTop: '64px'}
+                        
                         return (
-                            <React.Fragment>
-                                <p>Authenticated</p>
-                            </React.Fragment>
+                            <div className="flex h-screen">
+                                <ParamsNavigation 
+                                    activeMenu={activeMenu}
+                                />
+
+                                <div className="flex flex-col w-full mb-5" style={marginLeft}>
+                                    <ParamsUserBox />
+
+                                    <div className="w-full px-8 py-3 overflow-y-auto">
+                                        <div className="kiOAkj" style={marginTop}>
+
+                                            <Component {...props} />
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         )
                     } else {
                         return (
