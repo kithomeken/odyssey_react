45b5f3bb1c8fe0347fcf2628dee8e57d1067fd67
Kennedy Kithome <kithomeken@gmail.com>
2021-09-15 11:47:18 +0300

    Cookie Name For Authentication Token

--- src/components/router/Auth.js
+++ src/components/router/Auth.js
@@ -1,8 +1,11 @@
 import CookieService from '../../services/CookieService'
+import ConstantsRegistry from '../../global/ConstantsRegistry'
+
+const cookieNameForAccessToken = ConstantsRegistry.cookieNameForAccessToken()
 
 class Auth {
     constructor() {
-        const token = CookieService.get("")
+        const token = CookieService.get(cookieNameForAccessToken)
         token ? (this.authenticated = true) : (this.authenticated = false)
     }
 
