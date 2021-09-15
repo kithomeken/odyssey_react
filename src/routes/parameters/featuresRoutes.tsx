460931b33291b9aa8123de2e6058ca29c703f7c1
Kennedy Kithome <kithomeken@gmail.com>
2021-09-15 11:46:19 +0300

    Params Routes

---
+++ src/routes/parameters/featuresRoutes.tsx
@@ -0,0 +1,5 @@
+import SupportFeature from "../../views/parameters/features/SupportFeatures"
+
+export const featuresRoutes = [
+    { path: "/a/default/site-manager/features/support", component: SupportFeature, exact: true, activeMenu: 'support' },
+]
