3b7920dd78de072c6f834d6144340ef51c00c48a
Kennedy Kithome <kithomeken@gmail.com>
2021-09-16 17:30:03 +0300

    Security Routes URL Mod

--- src/routes/parameters/securityRoutes.tsx
+++ src/routes/parameters/securityRoutes.tsx
@@ -2,6 +2,6 @@ import AuthTeams from "../../views/parameters/security/AuthTeams";
 import CreateAuthTeams from "../../views/parameters/security/CreateAuthTeams";
 
 export const securityRoutes = [
-    { path: "/a/default/site-managr/security/auth-teams", component: AuthTeams, exact: true, activeMenu: 'teams' },
-    { path: "/a/default/site-managr/security/auth-teams/create", component: CreateAuthTeams, exact: true, activeMenu: 'teams' },
+    { path: "/a/default/site-manager/security/auth-teams", component: AuthTeams, exact: true, activeMenu: 'teams' },
+    { path: "/a/default/site-manager/security/auth-teams/create", component: CreateAuthTeams, exact: true, activeMenu: 'teams' },
 ]
