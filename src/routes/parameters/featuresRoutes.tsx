25572c108dec7243c2f5bc23034810c7e8e4fca5
Kennedy Kithome <kithomeken@gmail.com>
2021-09-27 08:53:21 +0300

    Security & Feature Routes Update

--- src/routes/parameters/featuresRoutes.tsx
+++ src/routes/parameters/featuresRoutes.tsx
@@ -1,5 +1,7 @@
 import SupportFeature from "../../views/parameters/features/SupportFeatures"
+import TicketFeatures from "../../views/parameters/features/TicketFeatures"
 
 export const featuresRoutes = [
     { path: "/a/default/site-manager/features/support", component: SupportFeature, exact: true, activeMenu: 'support' },
+    { path: "/a/default/site-manager/features/tickets", component: TicketFeatures, exact: true, activeMenu: 'tickets' },
 ]
--- src/routes/parameters/securityRoutes.tsx
+++ src/routes/parameters/securityRoutes.tsx
@@ -1,7 +1,10 @@
 import AuthTeams from "../../views/parameters/security/AuthTeams";
 import CreateAuthTeams from "../../views/parameters/security/CreateAuthTeams";
+import PasswordPolicies from "../../views/parameters/security/PasswordPolicies";
 
 export const securityRoutes = [
     { path: "/a/default/site-manager/security/auth-teams", component: AuthTeams, exact: true, activeMenu: 'teams' },
     { path: "/a/default/site-manager/security/auth-teams/create", component: CreateAuthTeams, exact: true, activeMenu: 'teams' },
+    
+    { path: "/a/default/site-manager/security/password", component: PasswordPolicies, exact: true, activeMenu: 'pwd-policies' },
 ]
