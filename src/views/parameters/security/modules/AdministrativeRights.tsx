bbd0a5a11e26888e9ac46b5141b5fc827feb7963
Kennedy Kithome <kithomeken@gmail.com>
2021-09-27 08:54:27 +0300

    Administrative Controls Update

--- src/views/parameters/security/modules/AdministrativeRights.tsx
+++ src/views/parameters/security/modules/AdministrativeRights.tsx
@@ -2,16 +2,14 @@ import React, {Component} from 'react'
 
 interface Props {
     inputs: any,
-    supportFeatures: any
+    supportFeatures: any,
+    onChangeHandler: any,
 }
 
 class AdministativeRights extends Component<Props> {
     state = {
         input: this.props.inputs,
         supportFeatures: this.props.supportFeatures,
-        adminRights: {
-
-        }
     }
 
     render() {
@@ -22,6 +20,44 @@ class AdministativeRights extends Component<Props> {
                         Issue out sets of administrative functionalities the users in this Auth Team can undertake.
                     </h2>
 
+                    {
+                        this.state.supportFeatures.client_access === 'Y' ? (
+                            <div className="ml-6">
+                                <div className="w-9/12 mb-4">
+                                    <div className="flex items-center">
+                                        <input
+                                            id="add_client"
+                                            name="add_client"
+                                            type="checkbox"
+                                            onChange={this.props.onChangeHandler}
+                                            checked={
+                                                this.state.input.ticket_access === 'GLB' ? (
+                                                    this.state.input.add_client === 'Y' ? true : false
+                                                    ) : (
+                                                    false
+                                                )
+                                            }
+                                            disabled={
+                                                this.state.input.ticket_access !== 'GLB' ? (
+                                                    true
+                                                ) : (
+                                                    false
+                                                )
+                                            }
+                                            className="h-4 w-4 mt-1 text-green-600 focus:ring-green-500 focus:bg-green-500 active: border-gray-300 rounded"
+                                        />
+
+                                        <label htmlFor="add_client" className="ml-4 block text-sm text-gray-500">
+                                            Can onboard new clients to the system
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                        ) : (
+                            null
+                        )
+                    }
+
                     <div className="w-full">
                         <div className="ml-6">
                             <div className="w-12/12 mb-4">
@@ -30,7 +66,21 @@ class AdministativeRights extends Component<Props> {
                                         id="add_users"
                                         name="add_users"
                                         type="checkbox"
-                                        onChange={this.onChangeHandler}
+                                        onChange={this.props.onChangeHandler}
+                                        checked={
+                                            this.state.input.ticket_access === 'GLB' ? (
+                                                this.state.input.add_users === 'Y' ? true : false
+                                                ) : (
+                                                false
+                                            )
+                                        }
+                                        disabled={
+                                            this.state.input.ticket_access !== 'GLB' ? (
+                                                true
+                                            ) : (
+                                                false
+                                            )
+                                        }
                                         className="h-4 w-4 mt-1 text-green-600 focus:ring-green-500 focus:bg-green-500 active: border-gray-300 rounded"
                                     />
 
@@ -40,30 +90,6 @@ class AdministativeRights extends Component<Props> {
                                 </div>
                             </div>
                         </div>
-                        
-                        {
-                            this.state.supportFeatures.client_access === 'Y' ? (
-                                <div className="ml-6">
-                                    <div className="w-9/12 mb-4">
-                                        <div className="flex items-center">
-                                            <input
-                                                id="add_client"
-                                                name="add_client"
-                                                type="checkbox"
-                                                onChange={this.onChangeHandler}
-                                                className="h-4 w-4 mt-1 text-green-600 focus:ring-green-500 focus:bg-green-500 active: border-gray-300 rounded"
-                                            />
-
-                                            <label htmlFor="add_client" className="ml-4 block text-sm text-gray-500">
-                                                Can onboard new clients to the system
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            ) : (
-                                null
-                            )
-                        }
 
                         <div className="ml-6">
                             <div className="w-12/12 mb-4">
@@ -72,12 +98,26 @@ class AdministativeRights extends Component<Props> {
                                         id="assign_auth_team"
                                         name="assign_auth_team"
                                         type="checkbox"
-                                        onChange={this.onChangeHandler}
+                                        onChange={this.props.onChangeHandler}
+                                        checked={
+                                            this.state.input.ticket_access === 'GLB' ? (
+                                                this.state.input.assign_auth_team === 'Y' ? true : false
+                                                ) : (
+                                                false
+                                            )
+                                        }
+                                        disabled={
+                                            this.state.input.ticket_access !== 'GLB' ? (
+                                                true
+                                            ) : (
+                                                false
+                                            )
+                                        }
                                         className="h-4 w-4 mt-1 text-green-600 focus:ring-green-500 focus:bg-green-500 active: border-gray-300 rounded"
                                     />
 
                                     <label htmlFor="assign_auth_team" className="ml-4 block text-sm text-gray-500">
-                                        Can assign different Auth Teams to users
+                                        Can change a user's Auth Team
                                     </label>
                                 </div>
                             </div>
@@ -90,7 +130,21 @@ class AdministativeRights extends Component<Props> {
                                         id="send_announcements"
                                         name="send_announcements"
                                         type="checkbox"
-                                        onChange={this.onChangeHandler}
+                                        onChange={this.props.onChangeHandler}
+                                        checked={
+                                            this.state.input.ticket_access === 'GLB' ? (
+                                                this.state.input.send_announcements === 'Y' ? true : false
+                                                ) : (
+                                                false
+                                            )
+                                        }
+                                        disabled={
+                                            this.state.input.ticket_access !== 'GLB' ? (
+                                                true
+                                            ) : (
+                                                false
+                                            )
+                                        }
                                         className="h-4 w-4 mt-1 text-green-600 focus:ring-green-500 focus:bg-green-500 active: border-gray-300 rounded"
                                     />
 
@@ -105,50 +159,37 @@ class AdministativeRights extends Component<Props> {
                             <div className="w-9/12 mb-4">
                                 <div className="flex items-center">
                                     <input
-                                        id="send_announcements"
-                                        name="send_announcements"
+                                        id="company_groups"
+                                        name="company_groups"
                                         type="checkbox"
-                                        onChange={this.onChangeHandler}
+                                        onChange={this.props.onChangeHandler}
+                                        checked={
+                                            this.state.input.ticket_access === 'GLB' ? (
+                                                this.state.input.company_groups === 'Y' ? true : false
+                                                ) : (
+                                                false
+                                            )
+                                        }
+                                        disabled={
+                                            this.state.input.ticket_access !== 'GLB' ? (
+                                                true
+                                            ) : (
+                                                false
+                                            )
+                                        }
                                         className="h-4 w-4 mt-1 text-green-600 focus:ring-green-500 focus:bg-green-500 active: border-gray-300 rounded"
                                     />
 
-                                    <label htmlFor="send_announcements" className="ml-4 block text-sm text-gray-500">
+                                    <label htmlFor="company_groups" className="ml-4 block text-sm text-gray-500">
                                         Can create client Company Groups
                                     </label>
                                 </div>
                             </div>
                         </div>
-
-
-
-
                     </div>
-
-
-
-
-
                 </div>
             </React.Fragment>
         )
-    }
-
-    onChangeHandler = (e: any) => {
-        let {ticketRights}: any = this.state
-        let isCheckbox: any = (e.target.type === 'checkbox') ? true : false;
-        ticketRights[e.target.name] = e.target.value
-
-        if (isCheckbox) {
-            if (e.target.checked) {
-                ticketRights[e.target.name] = "Y"
-            } else {
-                ticketRights[e.target.name] = "N"
-            }
-        }
-
-        this.setState({
-            ticketRights
-        })
     }
 }
 
