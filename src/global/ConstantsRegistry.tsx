0e826347a755c942800e92902263abefb25d3837
Kennedy Kithome <kithomeken@gmail.com>
2022-01-17 16:49:53 +0300

    Global Constants Registry

---
+++ src/global/ConstantsRegistry.tsx
@@ -0,0 +1,19 @@
+class ConstantsRegistry {
+    projectApplicationName() {
+        return 'Odyssey'
+    }
+
+    cookieNameForSanctumToken() {
+        return '._KO3rZlz0Zn'
+    }
+
+    cookieNameForAccountName() {
+        return '._z0QplQGhGm'
+    }
+
+    cookieNameForAccountEmail() {
+        return '._08gNXSM5Nw'
+    }
+}
+
+export default new ConstantsRegistry()
