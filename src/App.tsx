11f02565f695b358e389d83e57ba493cdf1fe087
Kennedy Kithome <kithomeken@gmail.com>
2022-01-17 16:55:24 +0300

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useContext, useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import { BrowserRouter as Router, Routes, Route, useLocation, useParams} from 'react-router-dom';
+
+import './assets/css/tailwind.css'
+import './assets/css/cuba.odyssey.css'
+import "react-toastify/dist/ReactToastify.css";
+import './assets/icons/fontawesome_pro/css/all.css'
+
+import Home from './pages/home/Home';
+import SignIn from './pages/auth/SignIn';
+import Error404 from './pages/errors/Error404';
+import ForgotPassword from './pages/auth/ForgotPassword';
+
+import FromIndexToHome from './lib/redirects/FromIndexToHome';
+import RequireAuthentication from './lib/router/RequireAuthentication';
+import PostAuthentication from './pages/auth/PostAuthentication';
+
+
+interface GuestRouteInterface {
+    path: string;
+    element: any;
+    caseSensitive?: boolean;
+}
+
+const guestRoutes: Array<GuestRouteInterface> = [
+    { path: "/auth/sign-in", element: <SignIn />, caseSensitive: true },
+    { path: "/auth/forgot-password", element: <ForgotPassword />, caseSensitive: true },
+]
+
+const redirectedRoutes = [
+    { path: "/", element: <FromIndexToHome />, activeMenu: 'Y' },
+    { path: "/home", element: <Home />, activeMenu: 'Y' },
+    { path: "/auth/post/access/ac/sntm/oen/seal/:uuid", element: <PostAuthentication />, activeMenu: 'Y' },
+]
+
+let protectedRoutes: Array<any> = []
+
+protectedRoutes = redirectedRoutes.concat(
+    // new routes
+);
+
+interface RouteContextType {
+    currentpage: string,
+    from: string
+}
+
+const RoutingContext = React.createContext<RouteContextType>(null!)
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    const RouterProvider = ({children}: {children: React.ReactNode}) => {
+        const currentLocation = useLocation()        
+        const [route, setRoute] = useState({ 
+            //--> it can be replaced with useRef or localStorage
+            currentpage: currentLocation.pathname,
+            from: ''
+        });
+        
+        useEffect(()=> {
+            setRoute((prev) => ({currentpage: currentLocation.pathname, from: prev.currentpage}) )
+        }, [currentLocation]);
+
+        return <RoutingContext.Provider value={route}>
+            {children}
+        </RoutingContext.Provider>
+    }
+    
+    const route = useContext(RoutingContext);
+
+    return (
+        <Router>
+            <RouterProvider>
+                <ToastContainer />
+
+                <Routes>
+                    {guestRoutes.map((route, index) => {
+                            return (
+                                <Route
+                                    path={route.path}
+                                    element={route.element}
+                                    key={index}
+                                />
+                            )
+                        })
+                    }
+                    
+                    <Route element={<RequireAuthentication />} >
+                        {
+                            protectedRoutes.map((route, index) => {
+                                return (
+                                    <Route
+                                        path={route.path}
+                                        element={route.element}
+                                        key={index}
+                                    />
+                                )
+                            })
+                        }
+                    </Route>
+
+                    <Route path="*" element={<Error404 />} />                
+                </Routes>
+            </RouterProvider>
+        </Router>
+    )
 }
 
 export default App;
+
