ffbb9a38d7480c1722b7d72a25d38ab9ee2814e6
Kennedy Kithome <kithomeken@gmail.com>
2022-01-19 02:34:00 +0300

    General Routes List

---
+++ src/routes/settings/generalRoutes.tsx
@@ -0,0 +1,5 @@
+import OrganizationalDetails from "../../pages/settings/general/organization/OrganizationalDetails";
+
+export const generalRoutes = [
+    { path: "/a/default/settings/general/organizational-details", element: <OrganizationalDetails />, activeMenu: 'organization' },
+]
