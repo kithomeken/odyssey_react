d7c0319591a470773911c8db2ff5d6dc2ec1ce30
Kennedy Kithome <kithomeken@gmail.com>
2022-01-29 21:33:58 +0300

    General Settings Routes Update

--- src/routes/settings/generalRoutes.tsx
+++ src/routes/settings/generalRoutes.tsx
@@ -1,5 +1,7 @@
+import Announcements from "../../pages/settings/general/announcements/Announcements";
 import OrganizationalDetails from "../../pages/settings/general/organization/OrganizationalDetails";
 
 export const generalRoutes = [
     { path: "/a/default/settings/general/organizational-details", element: <OrganizationalDetails />, activeMenu: 'organization' },
+    { path: "/a/default/settings/general/announcements", element: <Announcements />, activeMenu: 'announcements' },
 ]
