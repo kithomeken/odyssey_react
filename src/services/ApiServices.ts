10b3875fadc620b1cc13de7ab7c18bc28a620d72
Kennedy Kithome <kithomeken@gmail.com>
2022-05-14 18:51:08 +0300

    API Services Domain Update

--- src/services/ApiServices.ts
+++ src/services/ApiServices.ts
@@ -5,8 +5,11 @@ if (process.env.NODE_ENV === 'production') {
     domain = "https://project-0.kennedykitho.me"
     apiDomain = 'https://project-0.kennedykitho.me/api/'
 } else {
-    domain = 'http://localhost/project-0/public'
-    apiDomain = 'http://localhost/project-0/public/api/'
+    // domain = 'http://localhost/project-0/public'
+    // apiDomain = 'http://localhost/project-0/public/api/'
+
+    domain = "https://project-0.kennedykitho.me"
+    apiDomain = 'https://project-0.kennedykitho.me/api/'
 }
 
 class ApiServices {
