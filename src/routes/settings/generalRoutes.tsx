d1698da1b1e38c22b97887494c2811b9b0bd6001
Kennedy Kithome <kithomeken@gmail.com>
2022-05-31 13:14:09 +0300

    Product Management App Routes

--- src/routes/settings/generalRoutes.tsx
+++ src/routes/settings/generalRoutes.tsx
@@ -1,7 +1,13 @@
 import Announcements from "../../pages/settings/general/announcements/Announcements";
+import AddProduct from "../../pages/settings/general/organization/AddProduct";
 import OrganizationalDetails from "../../pages/settings/general/organization/OrganizationalDetails";
+import ProductManagement from "../../pages/settings/general/organization/ProductsManagements";
+import Productview from "../../pages/settings/general/organization/ProductView";
 
 export const generalRoutes = [
     { path: "/a/default/settings/general/organizational-details", element: <OrganizationalDetails />, activeMenu: 'organization' },
     { path: "/a/default/settings/general/announcements", element: <Announcements />, activeMenu: 'announcements' },
+    { path: "/a/default/settings/general/products", element: <ProductManagement />, activeMenu: 'products' },
+    { path: "/a/default/settings/general/products/new", element: <AddProduct />, activeMenu: 'products' },
+    { path: "/a/default/settings/general/products/:uuid", element: <Productview />, activeMenu: 'products' },
 ]
