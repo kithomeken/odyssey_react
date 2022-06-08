5dee07695e8cc9ea62d8b5e42cea20ec587d30fd
Kennedy Kithome <kithomeken@gmail.com>
2022-06-08 17:49:23 +0300

    Ticket Types & Company Groups API Routes

--- src/api/ApiRoutes.tsx
+++ src/api/ApiRoutes.tsx
@@ -9,3 +9,15 @@ export const PRODUCT_DECOMMISSION_API_ROUTE = 'portal/a/site-master/general/prod
 export const PRODUCT_REINSTATE_API_ROUTE = 'portal/a/site-master/general/products/reinstate'
 export const PRODUCT_B4_CHECK_API_ROUTE = 'portal/a/site-master/general/products/check-b4-update'
 export const PRODUCT_UPDATE_API_ROUTE = 'portal/a/site-master/general/products/update'
+
+// Company Groups
+export const COMPANY_GROUP_LIST_API_ROUTE = 'portal/a/site-master/general/company-groups/all'
+export const COMPANY_GROUP_CHECK_API_ROUTE = 'portal/a/site-master/general/company-groups/check'
+export const COMPANY_GROUP_CREATE_API_ROUTE = 'portal/a/site-master/general/company-groups/create'
+
+// Ticket Types
+export const TICKET_TYPES_LIST_API_ROUTE = 'portal/a/site-master/tickets/types/all'
+export const TICKET_TYPES_CHECK_API_ROUTE = 'portal/a/site-master/tickets/types/check'
+export const TICKET_TYPES_CREATE_API_ROUTE = 'portal/a/site-master/tickets/types/create'
+export const TICKET_TYPES_SUSPEND_API_ROUTE = 'portal/a/site-master/tickets/types/suspend'
+export const TICKET_TYPES_REINSTATE_API_ROUTE = 'portal/a/site-master/tickets/types/reinstate'
