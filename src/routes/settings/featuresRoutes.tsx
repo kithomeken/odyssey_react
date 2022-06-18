2cd9e56373b6ad14ce1c5d81cf40ff720647b4b7
Kennedy Kithome <kithomeken@gmail.com>
2022-06-18 20:47:25 +0300

    Ticket Status Routes

--- src/routes/settings/featuresRoutes.tsx
+++ src/routes/settings/featuresRoutes.tsx
@@ -1,13 +1,16 @@
 import SupportFeatures from "../../pages/settings/features/support/SupportFeatures";
 import CreateTicketType from "../../pages/settings/features/tickets/CreateTicketType";
 import TicketFeatures from "../../pages/settings/features/tickets/TicketFeatures";
+import TicketStatuses from "../../pages/settings/features/tickets/TicketStatuses";
 import TicketTypes from "../../pages/settings/features/tickets/TicketTypes";
 
 export const featuresRoutes = [
    /* 0 */ { path: "/a/default/settings/features/support", element: <SupportFeatures />, activeMenu: 'support' },
 
-   /* 1 */ { path: "/a/default/settings/features/ticket/types", element: <TicketTypes />, activeMenu: 'support' },
-   /* 2 */ { path: "/a/default/settings/features/ticket/types/create", element: <CreateTicketType />, activeMenu: 'support' },
+   /* 1 */ { path: "/a/default/settings/features/tickets/types", element: <TicketTypes />, activeMenu: 'support' },
+   /* 2 */ { path: "/a/default/settings/features/tickets/types/create", element: <CreateTicketType />, activeMenu: 'support' },
 
    /* 3 */ { path: "/a/default/settings/features/tickets", element: <TicketFeatures />, activeMenu: 'support' },
+
+   /* 4 */ { path: "/a/default/settings/features/tickets/status", element: <TicketStatuses />, activeMenu: 'support', name: 'STAT' },
 ]
