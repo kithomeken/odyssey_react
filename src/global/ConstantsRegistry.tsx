9493cee3cc929c8778cf2328c1e799d6071e2a75
Kennedy Kithome <kithomeken@gmail.com>
2022-06-18 20:50:04 +0300

    Constants Registry Update

--- src/global/ConstantsRegistry.tsx
+++ src/global/ConstantsRegistry.tsx
@@ -22,5 +22,8 @@ class ConstantsRegistry {
 
 export default new ConstantsRegistry()
 
+// New Registry
+export const APPLICATION_NAME = 'Odyssey'
+
 export const COOKIE_OPTIONS = {path: '/', secure: true, sameSite: 'none'}
 export const HEADER_SECTION_BG = 'bg-green-50'
