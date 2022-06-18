e2d80ffb64d220e4448fb6b42773d3474d4803f0
Kennedy Kithome <kithomeken@gmail.com>
2022-06-18 20:56:06 +0300

    Account Components & Routes Setup - Master Accounts

---
+++ src/routes/settings/accountRoutes.tsx
@@ -0,0 +1,6 @@
+import MasterAccounts from "../../pages/settings/accounts/MasterAccounts";
+
+export const accountRoutes = [
+    /* 0 */ { path: "/a/default/settings/accounts/master-accounts", element: <MasterAccounts />, activeMenu: 'master', name: 'MSTR' },
+     
+]
