5211ecbdd50dc6f9af696cc8d26c3b996be81f61
Kennedy Kithome <kithomeken@gmail.com>
2022-06-18 20:45:30 +0300

    Header Section Modification - Action Button Implementation

--- src/components/settings/Header.tsx
+++ src/components/settings/Header.tsx
@@ -1,5 +1,5 @@
-import React, {Component} from "react"
-import {Link} from 'react-router-dom'
+import React, { Component } from "react"
+import { Link } from 'react-router-dom'
 
 interface Props {
     title: string,
@@ -8,16 +8,20 @@ interface Props {
     buttonIcon?: boolean,
     iconType?: string,
     buttonLink?: any,
+    actionButton?: boolean,
+    actionEvent?: any,
 }
 
 class Header extends Component<Props> {
     render() {
-        const {title} = this.props
-        const {showButton} = this.props
-        const {buttonTitle} = this.props
-        const {buttonIcon} = this.props
-        const {iconType} = this.props
-        const {buttonLink} = this.props
+        const { title } = this.props
+        const { showButton } = this.props
+        const { buttonTitle } = this.props
+        const { buttonIcon } = this.props
+        const { iconType } = this.props
+        const { buttonLink } = this.props
+        const { actionButton } = this.props
+        const { actionEvent } = this.props
 
         return (
             <React.Fragment>
@@ -27,29 +31,54 @@ class Header extends Component<Props> {
                             {title}
                         </h2>
                     </div>
-                    
+
                     {
                         showButton ? (
-                            <div className="mt-5 flex lg:mt-0 lg:ml-4">
-                                <span className="hidden sm:block">
-                                    <Link to={buttonLink} type="button" className={`inline-flex items-center px-4 py-1-5 border-2 border-green-600 rounded shadow-sm text-sm text-green-700 hover:border-green-800 hover:text-green-800 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-green-400`}>
-                                        <span className="text-sm">
-                                            {buttonTitle}
-                                        </span>
-                                        {
-                                            buttonIcon ? (
-                                                <span className={`ml-2 ${iconType}`}></span>
-                                            ) : (
-                                                ''
-                                            )
-                                        }
-                                    </Link>
-                                </span>
-                            </div>
+                            <>
+                                {
+                                    actionButton ? (
+                                        // For header buttons with actions
+                                        // E.g. Opening a modal
+                                        <div className="mt-5 flex lg:mt-0 lg:ml-4">
+                                            <span className="hidden sm:block">
+                                                <button type="button" onClick={actionEvent} className={`inline-flex items-center px-4 py-1-5 border-2 border-green-600 rounded shadow-sm text-sm text-green-700 hover:border-green-800 hover:text-green-800 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-green-400`}>
+                                                    <span className="text-sm">
+                                                        {buttonTitle}
+                                                    </span>
+                                                    {
+                                                        buttonIcon ? (
+                                                            <span className={`ml-2 ${iconType}`}></span>
+                                                        ) : (
+                                                            ''
+                                                        )
+                                                    }
+                                                </button>
+                                            </span>
+                                        </div>
+                                    ) : (
+                                        <div className="mt-5 flex lg:mt-0 lg:ml-4">
+                                            <span className="hidden sm:block">
+                                                <Link to={buttonLink} type="button" className={`inline-flex items-center px-4 py-1-5 border-2 border-green-600 rounded shadow-sm text-sm text-green-700 hover:border-green-800 hover:text-green-800 focus:outline-none focus:ring focus:ring-offset-2 focus:ring-green-400`}>
+                                                    <span className="text-sm">
+                                                        {buttonTitle}
+                                                    </span>
+                                                    {
+                                                        buttonIcon ? (
+                                                            <span className={`ml-2 ${iconType}`}></span>
+                                                        ) : (
+                                                            ''
+                                                        )
+                                                    }
+                                                </Link>
+                                            </span>
+                                        </div>
+                                    )
+                                }
+                            </>
                         ) : (
                             null
                         )
-                    }    
+                    }
                 </div>
             </React.Fragment>
         )
