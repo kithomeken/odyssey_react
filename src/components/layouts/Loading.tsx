edafb65042ee59eb5d49d2236bc1173206996aec
Kennedy Kithome <kithomeken@gmail.com>
2022-06-27 20:25:11 +0300

    Loading Component Update

--- src/components/layouts/Loading.tsx
+++ src/components/layouts/Loading.tsx
@@ -4,7 +4,7 @@ class Loading extends Component {
     render() {
         return(
             <React.Fragment>
-                <div className="flex flex-col align-middle mt-6 h-16">
+                <div className="flex flex-col align-middle h-16">
                     <span className="fad text-green-500 fa-spinner-third fa-2x m-auto block fa-spin"></span>
                 </div>
             </React.Fragment>
