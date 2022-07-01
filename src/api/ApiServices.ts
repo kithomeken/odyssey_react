b1db028c865131073eb437f1ab419828d75c2bf7
Kennedy Kithome <kithomeken@gmail.com>
2022-07-01 14:23:40 +0300

    API Services - Image Domain

--- src/api/ApiServices.ts
+++ src/api/ApiServices.ts
@@ -1,16 +1,22 @@
 let apiDomain = ""
 let domain = ""
+let imagesDomain = ""
 
 if (process.env.NODE_ENV === 'production') {
     domain = "https://project-0.kennedykitho.me"
     apiDomain = 'https://project-0.kennedykitho.me/api/'
-} else {
-    // domain = 'http://localhost/project-0/public'
-    // apiDomain = 'http://localhost/project-0/public/api/'
 
-    domain = "https://project-0.kennedykitho.me"
-    apiDomain = 'https://project-0.kennedykitho.me/api/'
+    imagesDomain = 'https://images.kennedykitho.me/'
+} else {
+    domain = 'http://localhost/project-0/public'
+    apiDomain = 'http://localhost/project-0/public/api/'
+
+    // domain = "https://project-0.kennedykitho.me"
+    // apiDomain = 'https://project-0.kennedykitho.me/api/'
 }
+
+// TODO: Convert the API Services methods into export constants
+export const IMAGES_DOMAIN = imagesDomain
 
 class ApiServices {
     static apiDomain() {
