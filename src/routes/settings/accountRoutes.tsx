bc141297454c73c230749c824a7e0ebbf89edddd
Kennedy Kithome <kithomeken@gmail.com>
2022-08-08 11:33:13 +0300

    Agent Accounts Route

--- src/routes/settings/accountRoutes.tsx
+++ src/routes/settings/accountRoutes.tsx
@@ -1,6 +1,8 @@
+import { AgentAccounts } from "../../pages/settings/accounts/AgentAccounts";
 import MasterAccounts from "../../pages/settings/accounts/MasterAccounts";
 
 export const accountRoutes = [
     /* 0 */ { path: "/a/default/settings/accounts/master-accounts", element: <MasterAccounts />, activeMenu: 'master', name: 'MSTR' },
+    /* 1 */ { path: "/a/default/settings/accounts/agent-accounts", element: <AgentAccounts />, activeMenu: 'master', name: 'AGNT' },
      
 ]
