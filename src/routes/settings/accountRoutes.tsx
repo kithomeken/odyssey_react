e5b211e5e28e095319adedb8889a06a94974ee7f
Kennedy Kithome <kithomeken@gmail.com>
2022-08-10 20:52:47 +0300

    Account Routes Update - Agent Details

--- src/routes/settings/accountRoutes.tsx
+++ src/routes/settings/accountRoutes.tsx
@@ -1,8 +1,10 @@
 import { AgentAccounts } from "../../pages/settings/accounts/AgentAccounts";
+import { AgentDetails } from "../../pages/settings/accounts/AgentDetails";
 import MasterAccounts from "../../pages/settings/accounts/MasterAccounts";
 
 export const accountRoutes = [
-    /* 0 */ { path: "/a/default/settings/accounts/master-accounts", element: <MasterAccounts />, activeMenu: 'master', name: 'MSTR' },
-    /* 1 */ { path: "/a/default/settings/accounts/agent-accounts", element: <AgentAccounts />, activeMenu: 'master', name: 'AGNT' },
+    /* 0 */ { path: "/a/default/settings/user-accounts/master-accounts", element: <MasterAccounts />, activeMenu: 'master', name: 'MSTR' },
+    /* 1 */ { path: "/a/default/settings/user-accounts/agent-accounts", element: <AgentAccounts />, activeMenu: 'master', name: 'AGNT' },
+    /* 1 */ { path: "/a/default/settings/user-accounts/agent-accounts/:uuid", element: <AgentDetails />, activeMenu: 'master', name: 'AGNT' },
      
 ]
