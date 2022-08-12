d56aa825bcedc63bd63384fdc07d72c93570a302
Kennedy Kithome <kithomeken@gmail.com>
2022-08-12 08:41:15 +0300

    Ellipsis Drop Down

--- src/components/lib/DropDown.tsx
+++ src/components/lib/DropDown.tsx
@@ -8,12 +8,9 @@ function classNames(...classes) {
 
 interface Props {
     menuItems: any,
-    buttonTitle: any,
-    iconProperty?: any,
-    onMainActionButton: any
 }
 
-export const DropDown: FC<Props> = ({ buttonTitle, menuItems, onMainActionButton, iconProperty }) => {
+export const DropDown: FC<Props> = ({ menuItems }) => {
     function classNames(...classes) {
         return classes.filter(Boolean).join(' ')
     }
@@ -23,25 +20,15 @@ export const DropDown: FC<Props> = ({ buttonTitle, menuItems, onMainActionButton
             {({ open }) => (
                 <>
                     <div className='flex flex-row w-full'>
-                        <button
-                            type="button"
-                            onClick={onMainActionButton}
-                            className="inline-flex items-center px-3 py-1 rounded-l border-2 border-gray-300 shadow-sm text-sm text-gray-500 hover:border-gray-400 hover:text-gray-600 focus:outline-none focus:ring-0 focus:ring-offset-2 focus:ring-emerald-300">
-                            {
-                                iconProperty && (
-                                    <span className={`mr-2 ${iconProperty}`}></span>
-                                )
-                            }
-
-                            <span className="text-sm">
-                                {buttonTitle}
-                            </span>
-                        </button>
-
                         <Menu.Button
-                            className="flex justify-center w-6 rounded-r border-r-2 border-t-2 border-b-2 border-gray-300 shadow-sm px-1-5 py-1-5 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50 focus:outline-none focus:ring-0 focus:ring-offset focus:ring-offset-gray-100 focus:ring-emerald-500 align-middle">
+                            className={
+                                classNames(
+                                    open ? 'bg-gray-200' : null,
+                                    "flex justify-center w-6 rounded border-0 border-gray-300 shadow-sm py-1 bg-white text-sm font-medium text-gray-600 hover:bg-gray-200 focus:outline-none focus:ring-0 focus:ring-offset focus:ring-offset-gray-100 focus:ring-emerald-500 align-middle"
+                                )
+                            }>
                             <div className='h-5 w-5'>
-                                <span className="far fa-chevron-down text-gray-500 m-auto"></span>
+                                <span className="far fa-ellipsis-h fa-lg text-gray-500 m-auto"></span>
                             </div>
                         </Menu.Button>
                     </div>
