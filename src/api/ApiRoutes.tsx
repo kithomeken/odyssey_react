85973b203977251d0c31027f941cf9e86e0e95a2
Kennedy Kithome <kithomeken@gmail.com>
2022-08-16 15:07:37 +0300

    API Routes Update

--- src/api/ApiRoutes.tsx
+++ src/api/ApiRoutes.tsx
@@ -7,6 +7,7 @@ export const AGENT_CHANGE_EMAIL_INV_API_ROUTE = 'portal/a/site-master/accounts/u
 export const AGENT_RESEND_EMAIL_INV_API_ROUTE = 'portal/a/site-master/accounts/users/agents/invitations/resend'
 export const AGENT_AUTH_TEAM_LIST_API_ROUTE = 'portal/a/site-master/accounts/users/agents/authorization-teams/list'
 export const AGENT_CHANGE_AUTH_TEAM_API_ROUTE = 'portal/a/site-master/accounts/users/agents/authorization-teams/change'
+export const AGENT_SUSPEND_ACCOUNT_API_ROUTE = 'portal/a/site-master/accounts/users/agents/suspend'
 
 // Master Accounts
 export const MASTER_ACCOUNTS_LIST_API_ROUTE = 'portal/a/site-master/accounts/users/master/all'
