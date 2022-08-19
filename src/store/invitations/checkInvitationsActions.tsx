b1c76083a7b3ebe89afc2e4b808fe757cd7fccdd
Kennedy Kithome <kithomeken@gmail.com>
2022-08-19 20:49:30 +0300

    Check Invitations Actions & Reducers

---
+++ src/store/invitations/checkInvitationsActions.tsx
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+export const checkInvitationsActions = (encodedEndpointApi: any) => {
+    return (dispatch: (arg0: {type: string, response: any}) => void) => {
+        const decodedEndpointApi = decodeURIComponent(encodedEndpointApi)
+        
+        axios
+        .get(decodedEndpointApi)
+        .then((response) => {
+            if (response.data.success) {
+                dispatch({
+                    type: "INVITATION_IS_VALID",
+                    response: response.data
+                })
+            } else {
+                dispatch({
+                    type: "INVITATION_IS_NOT_VALID",
+                    response: response.data
+                })
+            }
+        })
+        .catch((error) => {            
+            dispatch({
+                type: "INVITATION_EXCEPTION_ERROR",
+                response: "Error while checking invitation link status",
+            });
+        })
+    }
+}
