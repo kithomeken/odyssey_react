123dbd474a95e21b33b9a3f988709068551819fb
Kennedy Kithome <kithomeken@gmail.com>
2022-08-20 17:07:20 +0300

    Guest Routes Restructure + Auto Authentication Sign In

--- src/routes/auth/guestRoutes.tsx
+++ src/routes/auth/guestRoutes.tsx
@@ -1,20 +1,29 @@
 import { AgentInvitation } from "../../pages/auth/AgentInvitation";
 import ForgotPassword from "../../pages/auth/ForgotPassword";
-import PostAuthentication from "../../pages/auth/PostAuthentication";
+import { PostInvitationAuth } from "../../pages/auth/PostInvitationAuth";
 import SignIn from "../../pages/auth/SignIn";
 
 interface GuestRouteInterface {
+    name: string,
     path: string;
     element: any;
     caseSensitive?: boolean;
 }
 
 export const guestRoutes: Array<GuestRouteInterface> = [
-    { path: "/auth/sign-in", element: <SignIn />, caseSensitive: true },
-    { path: "/auth/forgot-password", element: <ForgotPassword />, caseSensitive: true },
-
-    { path: "/api/u/auth/account/invtn/agn", element: <AgentInvitation />, caseSensitive: true },
+    { path: "/auth/sign-in", element: <SignIn />, caseSensitive: true, name: 'SIGN_IN' },
+    { path: "/auth/forgot-password", element: <ForgotPassword />, caseSensitive: true, name: 'FGRT_PWD' },
+    { path: "/api/u/auth/account/invtn/agn", element: <AgentInvitation />, caseSensitive: true, name: 'AGN_INV' },
+    { path: "/auth-auto/sign-in/post/inv", element: <PostInvitationAuth />, caseSensitive: true, name: 'POST_INVITATION_AUTH_' },
 ]
+
+
+
+
+
+
+
+
 
 // http://localhost:3000/u/account/set-up/invtn/5-agn/5453c4d4983b084bee4f0d063a97ea7a4e0d37fd7ceb4c61c864f842279ace10?expires=1660426192&signature=78b7f0714f538613fe98e40d74df93b8976a0ab71202f99d9243c18897955037
 
