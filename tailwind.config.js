721a21a6690deced8dba7a75efc64bcb5e8f66d8
Kennedy Kithome <kithomeken@gmail.com>
2022-08-20 17:11:19 +0300

    FlowBite Integration With Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,27 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {
-      backgroundColor: ['checked'],
-      borderColor: ['checked'],
-      opacity: ['disabled'],
-      textColor: ['visited'],
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            gray:   colors.gray,
+            green:  colors.green,
+            purple: colors.purple,
+        },
     },
-  },
-  plugins: [],
+    variants: {
+        extend: {
+            backgroundColor: ['checked'],
+            borderColor: ['checked'],
+            opacity: ['disabled'],
+            textColor: ['visited'],
+        },
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
+    content: [
+        'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+    ]
 }
