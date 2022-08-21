7da96c4db31d17f4198af0408a2bcc485c32b98e
Kennedy Kithome <kithomeken@gmail.com>
2022-08-21 13:15:25 +0300

    New Reusable Post Account Authentication

---
+++ src/store/auth/postAccountAuthenticationActions.tsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+import Crypto from "../../encryption/Crypto";
+import CookieServices from "../../services/CookieServices";
+import { SANCTUM_COOKIE_NAME } from "../../global/CookieNames";
+import { AUTHENTICATED_ACCOUNT_INFO } from "../../api/ApiRegistry";
+
+export const postAccountAuthenticationActions = () => {
+    return (dispatch: (argo: {type: string, response: any}) => void) => {
+        dispatch({
+            type: "ACCOUNT_INFO_LOADING_",
+            response: null,
+        });
+
+        const enSanctumToken = CookieServices.get(SANCTUM_COOKIE_NAME)
+        const deSanctumToken = Crypto.decryptDataUsingAES256(enSanctumToken)
+        const authorizationBearer = {
+            headers: {
+                Authorization: "Bearer " + deSanctumToken,
+            }
+        }
+
+        axios
+        .get(AUTHENTICATED_ACCOUNT_INFO, authorizationBearer)
+        .then((response) => {
+            if (response.data.success) {
+                dispatch({
+                    type: "ACCOUNT_INFO_LOADED_",
+                    response: response.data.payload,
+                });
+            } else {
+                dispatch({
+                    type: "ACCOUNT_INFO_ERROR_",
+                    response: null,
+                });
+            }
+        })
+        .catch((error) => {
+            dispatch({
+                type: "ACCOUNT_INFO_EXCEPTION_",
+                response: "User account is not authenticated",
+            });
+        })
+    }
+}
