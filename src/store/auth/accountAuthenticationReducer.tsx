4f4cec815f026f57ef21d00661fff796fed3a8a8
Kennedy Kithome <kithomeken@gmail.com>
2022-08-21 13:50:26 +0300

    Account Sign Out & Account Info Integration

--- src/store/auth/accountAuthenticationReducer.tsx
+++ src/store/auth/accountAuthenticationReducer.tsx
@@ -1,13 +1,15 @@
 import Crypto from "../../encryption/Crypto";
 import CookieServices from "../../services/CookieServices";
 import { COOKIE_OPTIONS } from "../../global/ConstantsRegistry";
-import { SANCTUM_COOKIE_NAME, UUID_COOKIE_NAME } from "../../global/CookieNames";
+import { ACCOUNT_INFO_COOKIE, SANCTUM_COOKIE_NAME, UUID_COOKIE_NAME } from "../../global/CookieNames";
 
 const initialState = {
     uaid: null,
     error: null,
+    email: null,
     redirect: false, /* Redirects back to sign in */
-    authenticating: false,
+    accountName: null,
+    processing: false,
     isAuthenticated: false,
 }
 
@@ -19,7 +21,7 @@ export const accountAuthenticationReducer = (state = initialState, action: any) 
                 uaid: null,
                 error: null,
                 redirect: true,
-                authenticating: false,
+                processing: false,
                 isAuthenticated: false,
             }
     
@@ -29,7 +31,7 @@ export const accountAuthenticationReducer = (state = initialState, action: any) 
                 uaid: null,
                 error: null,
                 redirect: false,
-                authenticating: true,
+                processing: true,
                 isAuthenticated: false,
             }
     
@@ -46,7 +48,7 @@ export const accountAuthenticationReducer = (state = initialState, action: any) 
                 uaid: enUuidToken,
                 error: null,
                 redirect: false,
-                authenticating: false,
+                processing: false,
                 isAuthenticated: true,
             }
 
@@ -56,11 +58,63 @@ export const accountAuthenticationReducer = (state = initialState, action: any) 
                 ...initialState,
                 uaid: null,
                 redirect: false,
-                authenticating: false,
+                processing: false,
                 isAuthenticated: false,
                 error: "Credentials provided did not match our records",
             }
 
+        case 'ACCOUNT_INFO_LOADING_':
+            return {
+                ...initialState,
+                email: null,
+                accountName: null,
+                processing: false,
+            }
+
+        case 'ACCOUNT_INFO_LOADING_':
+            const accountDetails = action.response.account
+            const enAccountDetails = Crypto.encryptDataUsingAES256(accountDetails)
+            CookieServices.set(ACCOUNT_INFO_COOKIE, enAccountDetails, COOKIE_OPTIONS)
+
+            return {
+                ...initialState,
+                email: accountDetails.email,
+                accountName: accountDetails.account_name,
+            }
+
+        case 'ACCOUNT_INFO_ERROR_':
+        case 'ACCOUNT_INFO_EXCEPTION_':
+            return {
+                ...initialState,
+                uaid: null,
+                email: null,
+                redirect: true,
+                accountName: null,
+                isAuthenticated: false,
+                error: "User account is not signed in"
+            }
+
+        case 'ACCOUNT_SIGN_OUT_PROCESSING_':
+            return {
+                ...initialState,
+                error: null,
+                redirect: false,
+                processing: true,
+            }
+
+        case 'ACCOUNT_SIGNED_OUT_':
+        case 'ACCOUNT_SIGN_OUT_EXCEPTION_':
+            return {
+                ...initialState,
+                uaid: null,
+                email: null,
+                error: null,
+                redirect: true,
+                accountName: null,
+                processing: false,
+                isAuthenticated: false,
+            }
+       
         default:
             return state;
     }
