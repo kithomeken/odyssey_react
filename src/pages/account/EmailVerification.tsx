e92e857879bc9a36eb4a05b544095549ccfaf727
Kennedy Kithome <kithomeken@gmail.com>
2022-08-27 20:25:28 +0300

    Email Verification Module

---
+++ src/pages/account/EmailVerification.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+export const EmailVerification = () => {
+    return (
+        <React.Fragment>
+            <Helmet>
+                <title>Email Verification</title>
+            </Helmet>
+
+        </React.Fragment>
+    )
+}
