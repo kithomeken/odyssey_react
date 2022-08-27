5e3d2164fb91a11826fd3d446efa3e0097d2c87a
Kennedy Kithome <kithomeken@gmail.com>
2022-08-27 20:23:17 +0300

    Account Security Module

---
+++ src/pages/account/AccountSecurity.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export const AccountSecurity = () => {
+    return (
+        <React.Fragment>
+            <div className="w-full">
+                <p className="text-lg text-slate-600 mb-3">
+                    Security
+                </p>
+
+
+
+
+            </div>
+        </React.Fragment>
+    )
+}
