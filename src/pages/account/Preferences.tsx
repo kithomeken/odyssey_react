efb0e0ae60f35da7ea34da2fece02a77a999da46
Kennedy Kithome <kithomeken@gmail.com>
2022-08-27 20:24:24 +0300

    Account Preferences Module

---
+++ src/pages/account/Preferences.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export const Preferences = () => {
+    return (
+        <React.Fragment>
+            <div className="w-full">
+                <p className="text-lg text-slate-600 mb-3">
+                    Preferences
+                </p>
+
+
+
+
+            </div>
+        </React.Fragment>
+    )
+}
