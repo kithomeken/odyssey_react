c8a47ef71dfb7210a9daa44e535dabbf1b6b7502
Kennedy Kithome <kithomeken@gmail.com>
2022-08-31 09:48:27 +0300

    Top Navbar Update

--- src/components/standard/TopNavigation.tsx
+++ src/components/standard/TopNavigation.tsx
@@ -1,9 +1,29 @@
-import React from "react"
+import { Menu } from "@headlessui/react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import { AccountDropDown } from "../global/AccountDropDown"
 import { SettingsDropDown } from "../global/SettingsDropDown"
 
 export const TopNavigation = () => {
+    const [state, setstate] = useState({
+        show: {
+            setting: false,
+        }
+    })
+
+    function classNames(...classes) {
+        return classes.filter(Boolean).join(' ')
+    }
+
+    const showOrHideMenuSettings = () => {
+        let {show} = state
+        show.setting = !state.show.setting
+
+        setstate({
+            ...state, show
+        })
+    }
+
     return (
         <React.Fragment>
             <nav className="bg-white shadow fixed w-full z-10">
@@ -43,7 +63,29 @@ export const TopNavigation = () => {
                             </button>
 
                             <div className="p-1 ml-3 rounded hover:text-slate-700 focus:outline-none focus:ring-0 focus:ring-offset-2 focus:ring-white">
-                                <SettingsDropDown />
+                                <Menu as="div" className="relative inline-block text-left float-right">
+                                    <div className='flex flex-row w-full'>
+                                        <Menu.Button
+                                        onClick={showOrHideMenuSettings}
+                                            className={
+                                                classNames(
+                                                    state.show.setting ? 'bg-slate-200 text-slate-700' : null,
+                                                    "flex justify-center w-auto px-1 rounded py-1 bg-white text-sm font-medium text-slate-500 hover:bg-slate-200 hover:text-slate-700 focus:outline-none focus:ring-0 focus:ring-offset focus:ring-offset-slate-100 focus:ring-emerald-500 align-middle"
+                                                )
+                                            }>
+                                            <div className='h-7 w-7'>
+                                                <svg xmlns="http://www.w3.org/2000/svg" x-description="Heroicon name: outline/cog" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                </svg>
+                                            </div>
+                                        </Menu.Button>
+                                    </div>
+
+                                    <SettingsDropDown
+                                        show={state.show.setting}
+                                    />
+                                </Menu>
                             </div>
 
                             <div className="ml-3 relative">
