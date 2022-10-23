a1fa5ce1859532c2f1d84470791318e6fbcd1d83
Kennedy Mutiso <kmutiso@co-opbank.co.ke>
2022-10-23 07:29:58 +0300

    New API Registry & Format

---
+++ src/api/v1/api.AccountRoutes.tsx
@@ -0,0 +1,11 @@
+/* 
+ * Account Management Routes
+*/
+
+export const AGENT_LIST_API_ROUTE = '/v1/settings/a/accounts/agents'
+export const AGENT_SUSPEND_ACCOUNT_API_ROUTE = '/v1/settings/a/accounts/agents/suspend'
+export const AGENT_RESTORE_ACCOUNT_API_ROUTE = '/v1/settings/a/accounts/agents/restore'
+export const AGENT_INVITATION_API_ROUTE = '/v1/settings/a/accounts/agents/invitations'
+export const AGENT_RESEND_EMAIL_INV_API_ROUTE = '/v1/settings/a/accounts/agents/invitations/resend'
+export const AGENT_AUTH_TEAM_LIST_API_ROUTE = '/v1/settings/a/accounts/agents/authorization-teams/list'
+export const AGENT_CHANGE_AUTH_TEAM_API_ROUTE = '/v1/settings/a/accounts/agents/authorization-teams/change'
---
+++ src/api/v1/api.GeneralRoutes.tsx
@@ -0,0 +1,15 @@
+/* 
+ * General Settings API Routes
+*/
+
+/* 
+ * Product Management API Routes
+*/
+
+export const PRODUCT_LIST_API_ROUTE = '/v1/settings/a/general/products'
+export const PRODUCT_ADD_API_ROUTE = '/v1/settings/a/general/products/create'
+export const PRODUCT_CHECK_API_ROUTE = '/v1/settings/a/general/products/check'
+export const PRODUCT_UPDATE_API_ROUTE = '/v1/settings/a/general/products/update'
+export const PRODUCT_REINSTATE_API_ROUTE = '/v1/settings/a/general/products/reinstate'
+export const PRODUCT_B4_CHECK_API_ROUTE = '/v1/settings/a/general/products/check-b4-update'
+export const PRODUCT_DECOMMISSION_API_ROUTE = '/v1/settings/a/general/products/decommission'
