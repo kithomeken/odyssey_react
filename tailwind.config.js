b10f9f79000ed84facc198c02765279f20cfa9f1
Kennedy Mutiso <kmutiso@CO-OPBANK.CO.KE>
2023-01-05 15:21:44 +0300

    Configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,8 @@ module.exports = {
         },
     },
     plugins: [],
-    content: []
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+        "./node_modules/react-tailwindcss-select/dist/index.esm.js"
+    ]
 }
